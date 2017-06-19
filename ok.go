4df8ad5caa5eda7bbda2abc4945f5e8ab7ef94d3
Ed Robinson <ed.robinson@reevoo.com>
2017-06-19 17:46:21 +0100

    Adds logging and port configuration

--- ok.go
+++ ok.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	log.WithFields(log.Fields{
+		"remote_address": r.RemoteAddr,
+		"method":         r.Method,
+		"url":            r.URL.Path,
+	}).Info()
 	fmt.Fprint(w, "ok")
 }
 
 func main() {
+	log.SetFormatter(&log.JSONFormatter{})
+	port := flag.Int("port", 8888, "port number")
+	flag.Parse()
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8888", nil)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 }
